0fddb531d68a006def1b8dda30c4ba530a7c9046
Ernesto Ledesma <ernestoledesma@MacBook-Air-de-Ernesto.local>
2019-10-08 07:36:33 -0300

    primer commit - creacion de form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './component/Header'
+import NuevaCita from './component/NuevaCita';
+
+
+class App extends Component  {
+
+  state = {}
+
+  render() {
+
+    return (
+
+      <div className="container">
+
+        <Header 
+          titulo="Administrador  de Pacientes Veterinaria"
+        />
+        <NuevaCita />
+
+      </div>
+        
+    );
+  }
+ 
+
 }
 
 export default App;
